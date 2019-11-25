56193df9f864e546fe1f6ab87494e0fa0fdfb66a
Diego Navarro <luis_d_100@hotmail.com>
2019-11-24 21:43:57 -0600

    react components, props, styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React, {Component} from 'react';
+
+import { CardList} from './components/card-list/card-list.component';
+
 import './App.css';
+import { from } from 'rxjs';
 
 class App extends Component{
   constructor(props) {
@@ -7,7 +11,6 @@ class App extends Component{
   
     this.state = { 
       monsters: []
-      
     };
   }
     componentDidMount() {
@@ -15,12 +18,13 @@ class App extends Component{
       .then( response => response.json())
       .then( users => this.setState({ monsters: users}));
     }
-    
-  
-  
+
   render(){
   return (
     <div className="App">
+      <CardList name="Usando el props">
+      <h1>Children Diego</h1>
+      </CardList>
       {this.state.monsters.map(monsters=>(
         <h1 key={monsters.id}> {monsters.name}</h1>
       ))}
