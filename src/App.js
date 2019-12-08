dc1928bc0072e3fb4ae2bec4e748d3ed88a86111
Diego Navarro <luis_d_100@hotmail.com>
2019-12-07 22:04:06 -0600

    solo puse estilos

--- src/App.js
+++ src/App.js
@@ -12,13 +12,17 @@ class App extends Component{
       monsters: [],
       searchField: ''
     };
+
+
   }
     componentDidMount() {
       fetch('https://jsonplaceholder.typicode.com/users')
       .then( response => response.json())
       .then( users => this.setState({ monsters: users}));
     }
-
+    handleChange = (e) => {
+      this.setState({ searchField: e.target.value})
+    }
   render(){
 
     const { monsters, searchField } = this.state;  //destructure
@@ -27,10 +31,10 @@ class App extends Component{
 
     return (
       <div className="App">
-     
+        <h1>Monster blogs</h1>
         <SearchBox 
         placeholder='search mosnter'
-        handleChange= {e => this.setState({ searchField: e.target.value})}
+        handleChange= {this.handleChange}
         
         />
         <CardList monsters={filteredMonsters} />
