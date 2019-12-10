ae7c381836b31394bb878315c608169dd16314ec
Diego Navarro <luis_d_100@hotmail.com>
2019-12-09 20:33:16 -0600

    Updates

---
+++ precache-manifest.a25f69c82f092b1df538951fb36f8219.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f761693b743413e2392b38f44fd0087",
+    "url": "/react-robo-blox/index.html"
+  },
+  {
+    "revision": "c76839236493f17391ff",
+    "url": "/react-robo-blox/static/css/main.d2c112da.chunk.css"
+  },
+  {
+    "revision": "5dad0a6c7cad238e9a48",
+    "url": "/react-robo-blox/static/js/2.69aaf1ce.chunk.js"
+  },
+  {
+    "revision": "c76839236493f17391ff",
+    "url": "/react-robo-blox/static/js/main.5fb3ad86.chunk.js"
+  },
+  {
+    "revision": "afc5f45a1ec40c96059f",
+    "url": "/react-robo-blox/static/js/runtime-main.95395b3f.js"
+  }
+]);
